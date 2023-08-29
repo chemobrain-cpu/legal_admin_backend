c8ff25f72e8cf46456a328669f32339b56423d3c
precious <arierhiprecious@gmail.com>
2023-08-29 05:55:58 -0700

    'latestcommit'

---
+++ controller/admin.js
@@ -0,0 +1,307 @@
+const express = require("express")
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken")
+const { generateAcessToken } = require('../utils/utils')
+const { Admin, Case } = require("../database/databaseConfig");
+const { validationResult } = require("express-validator");
+
+
+module.exports.getAdminFromJwt = async (req, res, next) => {
+   try {
+      let token = req.headers["header"]
+      if (!token) {
+         throw new Error("a token is needed ")
+      }
+      const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
+
+      const admin = await Admin.findOne({ email: decodedToken.email })
+
+      if (!admin) {
+         //if user does not exist return 404 response
+         return res.status(404).json({
+            response: "admin has been deleted"
+         })
+      }
+
+      return res.status(200).json({
+         response: {
+            admin: admin,
+         }
+      })
+
+   } catch (error) {
+      error.message = error.message || "an error occured try later"
+      return next(error)
+   }
+
+}
+
+
+module.exports.signup = async (req, res, next) => {
+   try {
+      //email verification
+      let { password, email, secretKey } = req.body
+      //checking for validation error
+      const errors = validationResult(req)
+
+      if (!errors.isEmpty()) {
+         let error = new Error("invalid user input")
+         return next(error)
+      }
+
+      //check if the email already exist
+      let adminExist = await Admin.findOne({ email: email })
+
+      if (adminExist) {
+         let error = new Error("admin is already registered")
+         //setting up the status code to correctly redirect user on the front-end
+         error.statusCode = 301
+         return next(error)
+      }
+
+
+      //check for secretkey
+      if (secretKey !== 'legal') {
+         let error = new Error("secretKey mismatched")
+         error.statusCode = 300
+         return next(error)
+      }
+
+
+      //hence proceed to create models of admin and token
+      let newAdmin = new Admin({
+         _id: new mongoose.Types.ObjectId(),
+         email: email,
+         password: password,
+      })
+
+      let savedAdmin = await newAdmin.save()
+
+      if (!savedAdmin) {
+         //cannot save user
+         let error = new Error("an error occured")
+         return next(error)
+      }
+
+      let token = generateAcessToken(email)
+
+      //at this point,return jwt token and expiry alongside the user credentials
+      return res.status(200).json({
+         response: {
+            admin: savedAdmin,
+            token: token,
+            expiresIn: '500',
+         }
+      })
+
+
+   } catch (error) {
+      console.log(error)
+      error.message = error.message || "an error occured try later"
+      return next(error)
+   }
+}
+
+//sign in user with different response pattern
+module.exports.login = async (req, res, next) => {
+   try {
+      console.log(req.body)
+      let { email, password } = req.body
+      //checking for validation error
+      const errors = validationResult(req)
+
+      if (!errors.isEmpty()) {
+         let error = new Error("invalid user input")
+         return next(error)
+      }
+
+      let adminExist = await Admin.findOne({ email: email })
+
+
+      if (!adminExist) {
+         return res.status(404).json({
+            response: "admin is not yet registered"
+         })
+      }
+
+
+
+      //check if password corresponds
+      if (adminExist.password != password) {
+         let error = new Error("Password does not match")
+         return next(error)
+      }
+
+      let token = generateAcessToken(email)
+
+      //at this point,return jwt token and expiry alongside the user credentials
+      return res.status(200).json({
+         response: {
+            admin: adminExist,
+            token: token,
+            expiresIn: '500',
+         }
+      })
+   } catch (error) {
+      console.log(error)
+      error.message = error.message || "an error occured try later"
+      return next(error)
+
+   }
+}
+
+
+module.exports.fetchCases = async (req, res,next) => {
+   try {
+      let cases = await Case.find()
+
+      if (!cases) {
+         let error = new Error("An error occured")
+         return next(error)
+      }
+
+   console.log(cases)
+      return res.status(200).json({
+         response: {
+            cases:cases
+         }
+      })
+   } catch (error) {
+      error.message = error.message || "an error occured try later"
+      return next(error)
+
+   }
+
+
+
+
+
+}
+
+module.exports.fetchCase = async (req, res,next) => {
+   try {
+
+      let caseId = req.params.id
+
+      let case_ = await CaseOne.findOne({ _id: caseId })
+
+      if (!case_) {
+         let error = new Error("casenot found")
+         return next(error)
+      }
+      return res.status(200).json({
+         response: {
+            case_
+         }
+      })
+   } catch (error) {
+      error.message = error.message || "an error occured try later"
+      return next(error)
+
+   }
+
+}
+
+module.exports.updateCase = async (req, res,next) => {
+
+   try {
+      let caseId = req.params.id
+
+      let {
+         caseOwner,
+         subject,
+         clientEmail,
+         clientPhoneNumber,
+         clientAddress,
+         caseNumber,
+         caseSubject,
+         caseCategory,
+         caseMatter,
+         status,
+         attorney,
+         courtCaseNumber,
+         stage1,
+         stage2,
+         stage3,
+         progress,
+         chargingCourt,
+         nextCaseDate,
+         dateAdded
+
+      } = req.body
+
+      let case_ = await Case.findOne({ _id: caseId })
+
+      if (!case_) {
+         let error = new Error("casenot found")
+         return next(error)
+      }
+
+      //update case here
+      case_.caseOwner = caseOwner || ''
+      case_.subject = subject || ''
+      case_.clientEmail = clientEmail || ''
+      case_.clientPhoneNumber = clientPhoneNumber || ''
+      case_.clientAddress= clientAddress  || ''
+      case_.caseNumber = caseNumber || ' '
+      case_.caseSubject = caseSubject || ' '
+      case_.caseCategory = caseCategory || ''
+      case_.caseMatter = caseMatter || ' '
+      case_.status = status || ' '
+      case_.attorney = attorney || ' '
+      case_.courtCaseNumber = courtCaseNumber || ' '
+      case_.stage1 = stage1 || ' '
+      case_.stage2 = stage2|| ' '
+      case_.stage3 = stage3 || ' '
+      case_.progress = progress || ''
+      case_.chargingCourt = chargingCourt || ' '
+      case_.nextCaseDate = nextCaseDate || ' '
+      case_.dateAdded = dateAdded || ''
+
+
+      let savedCase = await case_.save()
+
+      if(!savedCase){
+         let error = new Error("an error occured on the server")
+         return next(error)
+      }
+
+      console.log(savedCase)
+
+      return res.status(200).json({
+         response:savedCase
+      })
+   } catch (error) {
+      error.message = error.message || "an error occured try later"
+      return next(error)
+
+   }
+
+}
+
+module.exports.deleteCase = async (req, res,next) => {
+   try {
+      let caseId = req.params.id
+      let case_ = await Case.deleteOne({ _id: caseId })
+      if (!case_) {
+         let error = new Error("an error occured")
+         return next(error)
+      }
+      return res.status(200).json({
+         response: {
+            message:'deleted successfully'
+         }
+      })
+   } catch(error) {
+      error.message = error.message || "an error occured try later"
+      return next(error)
+
+   }
+
+
+}
+
+
+
+
+
---
+++ routes/admin.js
@@ -0,0 +1,75 @@
+const express = require("express")
+const router = express.Router()
+const app = express()
+const ejs = require("ejs")
+const bodyParser = require("body-parser")
+const path = require("path")
+const fs = require("fs")
+const { verifyAdmin} = require("../utils/utils")
+const { body, validationResult, Result } = require('express-validator')
+
+
+let login = require("../controller/admin").login
+let signup = require("../controller/admin").signup
+
+
+let fetchCase = require("../controller/admin").fetchCase
+
+let fetchCases = require("../controller/admin").fetchCases
+
+let updateCase = require('../controller/admin').updateCase
+
+let getAdminFromJwt = require('../controller/admin').getAdminFromJwt
+
+let deleteCase = require('../controller/admin').deleteCase
+
+
+router.get("/adminbytoken", getAdminFromJwt)
+
+router.post("/adminlogin",
+[
+    body("email")
+        .isEmail()
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage("email is required"),
+    body("password")
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage("password is required"),
+],login)
+
+router.post('/adminsignup',
+[
+    body("secretKey")
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage("fullName is required"),
+    body("password")
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage("password is required"),
+    body("email")
+        .isEmail()
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage("email is required"),
+],signup)
+
+
+
+router.get('/auth/cases',verifyAdmin,fetchCases)
+
+router.get('/auth/cases/:id',verifyAdmin,fetchCase)
+
+router.patch('/auth/case/:id',verifyAdmin,updateCase)
+
+router.delete('/auth/case/:id',verifyAdmin,deleteCase)
+
+
+exports.router = router
